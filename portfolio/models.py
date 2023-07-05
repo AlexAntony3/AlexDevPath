68780dbf31c8c507da806fc40c4bb481d8b8d430
AlexAntony3 <alex.antony.jy@gmail.com>
2023-07-05 20:15:05 +0000

    feat: add the about page model

--- portfolio/models.py
+++ portfolio/models.py
@@ -9,3 +9,13 @@ class Home(models.Model):
     intro_description = models.TextField()
 
 
+class About(models.Model):
+    title = models.CharField(max_length=50, blank=True)
+    second_image = models.CloudinaryField('image')
+    first_name = CharField(max_length=80, unique=True)
+    last_name = CharField(max_length=80, unique=True)
+    age = models.IntegerField(null=True, blank=True)
+    nationality = models.CharField(max_length=200)
+    languages = models.CharField(max_length=200)
+    address = models.CharField(max_length=400, unique=True)
+    freelance = models.BooleanField(default=False)
