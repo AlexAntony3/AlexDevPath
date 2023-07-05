75e66380efab81d1628bee9ec1e39b943a281305
AlexAntony3 <alex.antony.jy@gmail.com>
2023-07-05 19:47:39 +0000

    feat: add a home page model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Home(models.Model):
+    title = models.CharField(max_length=200, blank=True)
+    main_image = models.CloudinaryField('image')
+    name_header = models.CharField(max_length=100)
+    intro_description = models.TextField()
+
+
