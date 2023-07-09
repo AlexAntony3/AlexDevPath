57a6b056b19b24d655f3055c0a8f5c9c90354fcc
AlexAntony3 <alex.antony.jy@gmail.com>
2023-07-09 17:07:54 +0000

    feat: create admin panel fields

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Home, About, Project, Skill, Experience, Education, Contact
 
-# Register your models here.
+
+admin.site.register(Home)
+admin.site.register(About)
+admin.site.register(Project)
+admin.site.register(Skill)
+admin.site.register(Experience)
+admin.site.register(Education)
+admin.site.register(Contact)
