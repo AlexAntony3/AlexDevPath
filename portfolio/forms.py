adec21094e5a4aa10f3563d60abf6af4e0358d04
AlexAntony3 <alex.antony.jy@gmail.com>
2023-09-04 21:56:34 +0000

    feat: create all forms required

---
+++ portfolio/forms.py
@@ -0,0 +1,47 @@
+from .models import About, Project, Skill, Experience, Education
+from django import forms
+
+
+class AboutForm(forms.ModelForm):
+    class Meta:
+        model = About
+        fields = ['second_image',
+                  'age',
+                  'nationality',
+                  'languages',
+                  'address',
+                  'freelane']
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['thumbnail',
+                  'skills',
+                  'description',
+                  'live_url',
+                  'github_URL',]
+
+
+class SkillForm(forms.ModelForm):
+    class Meta:
+        model = Skill
+        fields = ['name',
+                  'proficiency',
+                  'icon',]
+
+
+class Experience(forms.ModelForm):
+    class Meta:
+        model = Experience
+        fields = ['company',
+                  'start_date',
+                  'description']
+
+
+class Education(forms.ModelForm):
+    class Meta:
+        model = Education
+        fields = ['qualification',
+                  'start_date',
+                  'description']
